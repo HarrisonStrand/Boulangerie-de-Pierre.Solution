ff6e9bb2ba18bae31c8058ca6cbb1ff25d9f7ce0
HarrisonStrand <harrisonstrand@gmail.com>
2020-12-11 10:05:04 -0800

    add purposeful fail for creating instance of Bread

---
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -0,0 +1,18 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+// using System.Collections.Generic;
+using Bakery.Models;
+// using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class BreadTests
+  {
+    [TestMethod]
+    public void Bread_CreatesInstanceOfBread_Bread()
+    {
+      Bread newBread = new Bread(1);
+      Assert.AreEqual(typeof(Bread), newBread.GetType());
+    }
+  }
+}
