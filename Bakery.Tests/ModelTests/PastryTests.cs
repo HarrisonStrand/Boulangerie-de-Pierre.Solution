59352efdad27ab0de492c07473141f07de267f99
HarrisonStrand <harrisonstrand@gmail.com>
2020-12-11 11:11:45 -0800

    add purposeful fail for generating instance of Pastry

---
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -0,0 +1,68 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using Bakery.Models;
+using System;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class PastryTests 
+  {
+    // public void Dispose()
+    // {
+    //   Pastry.ClearAll();
+    // }
+    [TestMethod]
+    public void Pastry_CreatesInstanceOfPastry_Pastry()
+    {
+      Pastry newPastry = new Pastry("Pastry", 5);
+      Assert.AreEqual(typeof(Pastry), newPastry.GetType());
+    }
+    // [TestMethod]
+    // public void GetDescription_ReturnsPastryDescription_String()
+    // {
+    //   string description = "this is a Pastry";
+    //   int price = 5;
+    //   Pastry newPastry = new Pastry(description, price);
+    //   string result = newPastry.PastryDescription;
+    //   Assert.AreEqual(description, result);
+    // }
+    // [TestMethod]
+    // public void GetPrice_ReturnsPastryPrice_Int()
+    // {
+    //   string description = "this is a Pastry";
+    //   int price = 5;
+    //   Pastry newPastry = new Pastry(description, price);
+    //   int result = newPastry.PastryPrice;
+    //   Assert.AreEqual(price, result);
+    // }
+    // [TestMethod]
+    // public void SetPrice_SetPastryPrice_Int()
+    // {
+    //   string description = "this is a Pastry";
+    //   int price = 5;
+    //   Pastry newPastry = new Pastry(description, price);
+    //   int updatedPrice = 10;
+    //   newPastry.PastryPrice = updatedPrice;
+    //   int result = newPastry.PastryPrice;
+    //   Assert.AreEqual(updatedPrice, result);
+    // }
+    // [TestMethod]
+    // public void GetAll_ReturnsEmptyList_PastryList()
+    // {
+    //   List<Pastry> newList = new List<Pastry> { };
+    //   List<Pastry> result = Pastry.GetAll();
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+    // [TestMethod]
+    // public void GetAll_ReturnsItems_PastryList()
+    // {
+    //   string description = "this is surely a Pastry";
+    //   int price = 5;
+    //   Pastry newPastry = new Pastry(description, price);
+    //   List<Pastry> newList = new List<Pastry> { newPastry };
+    //   List<Pastry> result = Pastry.GetAll();
+    //   CollectionAssert.AreEqual(newList, result);
+    // }
+  }
+}
---
+++ Bakery/Models/Pastry.cs
@@ -0,0 +1,29 @@
+using System.Collections.Generic;
+using System;
+
+namespace Bakery.Models 
+{
+  public class Pastry
+  {
+
+    public string PastryDescription { get; set; }
+    public int PastryPrice { get;set; }
+
+    private static List<Pastry> _pastryList = new List<Pastry> { }; 
+
+    public Pastr(string description, int price)
+    {
+      PastryDescription = description;
+      PastryPrice = price;
+      _pastryList.Add(this);
+    }
+    public static List<Pastry> GetAll()
+    {
+      return _pastryList;
+    }
+    public static void ClearAll()
+    {
+      _pastryList.Clear();
+    }
+  }
+}
